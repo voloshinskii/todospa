e312a50ef35abc3e4a2d62e607b3ce6506d41a67
Max Voloshinskii <me@voloshinskii.ru>
2019-09-21 20:45:21 +0300

    Updates

---
+++ precache-manifest.2b5d13de94e3e814082189f0a79817e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dfe9c51a9bb9ae94fb95",
+    "url": "/static/js/main.623c1af0.chunk.js"
+  },
+  {
+    "revision": "c039057ca7c097259ab5",
+    "url": "/static/js/2.0c434e52.chunk.js"
+  },
+  {
+    "revision": "dfe9c51a9bb9ae94fb95",
+    "url": "/static/css/main.af4b7f0c.chunk.css"
+  },
+  {
+    "revision": "c039057ca7c097259ab5",
+    "url": "/static/css/2.0d0953d3.chunk.css"
+  },
+  {
+    "revision": "82e20993eaeac56a5c17a352eed5249c",
+    "url": "/index.html"
+  }
+];
